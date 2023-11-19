6b57633543ada752ee246c841031c524f38569ec
Umar Mian <umarmian@uflip.com>
2023-11-19 13:01:23 -0600

    stopped at lecture 17

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,81 @@
+// *****************************
+// Lecture: Values and Variables
+// *****************************
+
+// **** Challenge ****
+// let country = "Pakistan";
+// let continent = "Asia";
+// let population = "231.4 million";
+
+// console.log(
+//   'I am from "' +
+//     country +
+//     '" located in "' +
+//     continent +
+//     '" with a current population of "' +
+//     population +
+//     '"'
+// );
+// const headerEl = document.querySelector("h1");
+
+// headerEl.addEventListener("click", function () {
+//   headerEl.textContent =
+//     "I am from '" +
+//     country +
+//     '" located in "' +
+//     continent +
+//     '" with a current population of "' +
+//     population +
+//     '"';
+//   headerEl.style.backgroundColor = "#252525";
+//   headerEl.style.padding = "5rem";
+//   headerEl.style.borderRadius = "100px";
+// });
+
+// ----------------------------------------------------------------------------->
+
+// *******************
+// Lecture: Data Types
+// *******************
+
+// console.log(typeof null);
+
+// // **** Challenge ****
+// let isIsland = false;
+
+// console.log(typeof isIsland);
+// console.log(typeof population);
+// console.log(typeof country);
+
+// ----------------------------------------------------------------------------->
+
+// // ****************************
+// // Lecture: let, const, and var
+// // ****************************
+
+// // **** Challenge ****
+// const language = "Urdu";
+// console.log(language);
+
+// ----------------------------------------------------------------------------->
+
+// ************************
+// Lecture: Basic Operators
+// ************************
+
+// const ageUmar = 2023 - 2002;
+// console.log(ageUmar);
+
+// const firstName = "Umar";
+// const lastName = "Mian";
+// const space = " ";
+// console.log(firstName + space + lastName);
+// console.log(firstName + " " + lastName);
+
+// let x = 10 + 5; // x is 15
+// x += 10; // += means x = x + 10; so that means x = 15 (from prev x) + 10
+// console.log(x);
+
+// ****************************
+// Lecture: Operators Precdence
+// ****************************
